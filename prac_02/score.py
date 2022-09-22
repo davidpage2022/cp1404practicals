689c1a05caa3fdede1d485d0e4b7f9547b0e08a8
David C. Page <davidpage876@gmail.com>
2022-09-22 13:17:13 +1000

    Reformat score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,23 @@ CP1404/CP5632 - Practical
 Determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Pass")
-else:
-    print("Excellent")
+
+def main():
+    """Ask the user for their score, then print the result"""
+    score = float(input("Enter score: "))
+    print(get_result(score))
+
+
+def get_result(score):
+    """Returns the result description for a score value between 0 and 100"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Pass"
+    else:
+        return "Excellent"
+
+
+main()
