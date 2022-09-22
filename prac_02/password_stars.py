f001bdca9cd39a6d4a9af288e885d1c4297d64e5
David C. Page <davidpage876@gmail.com>
2022-09-22 12:56:11 +1000

    Refactor into main() function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,14 @@
+"""Check that passwords are long enough, then display them as stars (e.g. *********). """
 MINIMUM_LENGTH = 8
-password = input("Enter password: ")
-while len(password) < MINIMUM_LENGTH:
-    print(f"Password must be at least {MINIMUM_LENGTH} characters long")
+
+
+def main():
+    """Validate user input, then print as stars."""
     password = input("Enter password: ")
-print("*" * len(password))
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password must be at least {MINIMUM_LENGTH} characters long")
+        password = input("Enter password: ")
+    print("*" * len(password))
+
+
+main()
