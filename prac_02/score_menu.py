9d7391ba79900ec608d520f69ef2dc136903f608
David C. Page <davidpage876@gmail.com>
2022-09-25 19:53:04 +1000

    Add score menu program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,2 +1,48 @@
-"""Score menu program"""
+"""Program for interacting with scores"""
 
+MENU = """(G)et score
+(P)rint result
+Print (s)tars
+(Q)uit"""
+
+
+def main():
+    """Program for interacting with scores"""
+    score = 0
+    print(MENU)
+    choice = input(">>>").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = int(input("Enter score: "))
+            while score < 0 or score > 100:
+                print("Invalid score")
+                score = int(input("Enter score: "))
+        elif choice == "P":
+            print(get_result(score))
+        elif choice == "S":
+            print_stars(score)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>>").upper()
+    print("Thank you")
+
+
+def get_result(score):
+    """Returns the result description for a score value between 0 and 100"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Pass"
+    else:
+        return "Excellent"
+
+
+def print_stars(score):
+    """Print as many stars as the score"""
+    print("*" * score)
+
+
+main()
