81b4e1d99cea8b47ca5ffce1a0abe7efdb154468
David C. Page <davidpage876@gmail.com>
2022-10-01 12:26:11 +1000

    Complete files exercise

--- prac_03/files.py
+++ prac_03/files.py
@@ -16,20 +16,27 @@ def main():
     # Exercise 2:
     # Open "name.txt" and read the name (as above) then prints, "Your name is Bob" (Bob is name).
     in_file = open(f"{name}.txt", "r")
+    name = in_file.readline()
     print(f"Your name is {name}")
     in_file.close()
 
     # Exercise 3:
     # Open "numbers.txt", reads only the first two numbers and add them together then prints the result (59).
     in_file = open("numbers.txt", "r")
-    sum = 0
+    total = 0
     for i in range(LINES_TO_READ):
-        sum += int(in_file.readline())
-    print(f"The sum of the first {LINES_TO_READ} numbers is: {sum}")
+        total += int(in_file.readline())
     in_file.close()
+    print(f"The total of the first {LINES_TO_READ} numbers is: {total}")
 
     # Exercise 4:
-    # Todo
+    # Print the total for all lines in numbers.txt or a file with any number of numbers.
+    in_file = open("numbers.txt", "r")
+    total = 0
+    for line in in_file:
+        total += int(line)
+    in_file.close()
+    print(f"The total of all lines is: {total}")
 
 
 main()
