ec85f7e592067e2740bcf13a37c99deca2ab9193
David C. Page <davidpage876@gmail.com>
2022-10-14 21:02:26 +1000

    Add structure for quick_picks module

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,9 @@
+"""Generate lottery "quick picks" based on user input."""
+
+
+def main():
+    """ Ask user how many "Quick picks" they wish to generate then output the generated picks. """
+    pass
+
+
+main()
