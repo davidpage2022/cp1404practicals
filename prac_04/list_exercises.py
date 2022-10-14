357ef77a1ccb0d260a63fd24615c9d4189138cd9
David C. Page <davidpage876@gmail.com>
2022-10-14 20:29:32 +1000

    Create basic structure for list operations exercise program

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,18 @@
+"""Program that prompts the user for 5 numbers and then displays information about them."""
+
+
+def main():
+    """Prompt the user for 5 numbers and then displays information about them."""
+    numbers = get_numbers()
+    print_number_info(numbers)
+
+
+def get_numbers():
+    return []
+
+
+def print_number_info(numbers):
+    pass
+
+
+main()
