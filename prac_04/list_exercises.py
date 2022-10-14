bffc0862a5344af9637071647493d8f4403cad0a
David C. Page <davidpage876@gmail.com>
2022-10-14 20:36:38 +1000

    Display information about list of numbers

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,11 +8,21 @@ def main():
 
 
 def get_numbers():
-    return []
+    """Get 5 numbers from the user, returned as a list."""
+    numbers = []
+    for i in range(5):
+        number = int(input("Number: "))  # TODO: Support float input.
+        numbers.append(number)
+    return numbers
 
 
 def print_number_info(numbers):
-    pass
+    """Print information about a list of numbers"""
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {(sum(numbers) / len(numbers))}")
 
 
 main()
