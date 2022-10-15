bd07a286c6a93e0462c0caeb97c47faff2a34029
David C. Page <davidpage876@gmail.com>
2022-10-15 15:06:47 +1000

    Generate list of random picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,9 +1,22 @@
 """Generate lottery "quick picks" based on user input."""
 
+import random
+
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+NUMBERS_PER_PICK = 6
+
 
 def main():
-    """ Ask user how many "Quick picks" they wish to generate then output the generated picks. """
-    pass
+    """Ask user how many "Quick picks" they wish to generate then output the generated picks."""
+    number_of_picks = int(input("How many quick picks? "))
+
+    lines = []
+    for i in range(number_of_picks):
+        picks = [random.randint(MIN_NUMBER, MAX_NUMBER) for _ in range(NUMBERS_PER_PICK)]
+        lines.append(picks)
+
+    print(lines)
 
 
 main()
