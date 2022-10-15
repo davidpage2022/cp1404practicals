f46993c68f12e636f2c5b69d9dd7c6a7addd2b49
David C. Page <davidpage876@gmail.com>
2022-10-15 15:25:05 +1000

    Refactor into separate functions

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,13 +9,27 @@ NUMBERS_PER_LINE = 6
 
 def main():
     """Ask user how many "Quick picks" they wish to generate then output the generated picks."""
-    number_of_picks = int(input("How many quick picks? "))
+    number_of_picks = get_number_of_picks()
+    lines = generate_lines(number_of_picks)
+    display_lines(lines)
 
+
+def get_number_of_picks():
+    """Return the number "quick picks" to use, obtained from the user."""
+    return int(input("How many quick picks? "))
+
+
+def generate_lines(number_of_lines):
+    """Generate random lines ("quick picks")."""
     lines = []
-    for i in range(number_of_picks):
+    for i in range(number_of_lines):
         numbers = [random.randint(MIN_NUMBER, MAX_NUMBER) for _ in range(NUMBERS_PER_LINE)]
         lines.append(numbers)
+    return lines
 
+
+def display_lines(lines):
+    """Display lines, formatted into columns."""
     for line in lines:
         print(" ".join(f"{number:2}" for number in line))
 
