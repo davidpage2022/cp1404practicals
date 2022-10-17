ea352e8e6b654e4b1e80650f3b32dd40a04f1bc1
David C. Page <davidpage876@gmail.com>
2022-10-18 09:11:23 +1000

    Display word occurrences sorted

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -9,11 +9,15 @@ def main():
     """Count the occurrences of words in a string."""
     string = "this is a collection of words of nice words this is a fun thing it is"  # input("Text: ")
 
+    # Count word occurrences.
     words = string.split()
     unique_words = set(words)
     word_to_lengths = {}
     for word in unique_words:
         word_to_lengths[word] = words.count(word)
+
+    # Display sorted.
+    for word in sorted(word_to_lengths.keys()):
         print(f"{word} : {word_to_lengths[word]}")
 
 
