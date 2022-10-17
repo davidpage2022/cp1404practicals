4fc54653f302f2753427d5b9f8329a2f6a8b5269
David C. Page <davidpage876@gmail.com>
2022-10-18 09:50:18 +1000

    Refactor into separate functions

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -10,26 +10,31 @@ def main():
     email_to_names = {}
     email = input("Email: ")
     while email != "":
-
-        # Attempt to extract name from email, otherwise ask user for name.
-        name = extract_name(email)
-        choice = input(f"Is your name {name}? (Y/n)").upper()
-        if choice != "Y" and choice != "":
-            name = input("Name: ")
-
-        email_to_names[email] = name
+        email_to_names[email] = get_name(email)
         email = input("Email: ")
+    display(email_to_names)
 
-    # Display name and emails entered.
-    print()
-    for email, name in email_to_names.items():
-        print(f"{name} ({email})")
+
+def get_name(email):
+    """Attempt to extract name from email, otherwise ask user for name."""
+    name = extract_name(email)
+    choice = input(f"Is your name {name}? (Y/n)").upper()
+    if choice != "Y" and choice != "":
+        name = input("Name: ")
+    return name
 
 
 def extract_name(email):
-    """Attempt to extract a name from an email (e.g. john.doe@gmail.com returns "John Doe")"""
+    """Extract a name from an email (e.g. john.doe@gmail.com returns "John Doe")."""
     name = " ".join(email.split("@")[0].split("."))
     return name.title()
+
+
+def display(email_to_names):
+    """Display names and emails entered."""
+    print()
+    for email, name in email_to_names.items():
+        print(f"{name} ({email})")
 
 
 main()
