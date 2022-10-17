80b568d871d46eb7e6ee570739f8b10f97c2f409
David C. Page <davidpage876@gmail.com>
2022-10-18 09:46:13 +1000

    Attempt to extract name from email automatically

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -14,20 +14,22 @@ def main():
         # Attempt to extract name from email, otherwise ask user for name.
         name = extract_name(email)
         choice = input(f"Is your name {name}? (Y/n)").upper()
-        if choice != "Y":
+        if choice != "Y" and choice != "":
             name = input("Name: ")
 
         email_to_names[email] = name
         email = input("Email: ")
 
     # Display name and emails entered.
+    print()
     for email, name in email_to_names.items():
         print(f"{name} ({email})")
 
 
 def extract_name(email):
     """Attempt to extract a name from an email (e.g. john.doe@gmail.com returns "John Doe")"""
-    return "John Doe"
+    name = " ".join(email.split("@")[0].split("."))
+    return name.title()
 
 
 main()
