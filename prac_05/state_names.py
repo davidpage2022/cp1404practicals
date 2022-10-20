d62ab5dc0de143e9702dfd205bb6c8fdb26ee886
David C. Page <davidpage876@gmail.com>
2022-10-20 10:51:16 +1000

    Refactor based on code review feedback

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,13 +6,13 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-for state_code, name in CODE_TO_NAME.items():
-    print(f"{state_code:3} is {name}")
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
+short_code = input("Enter short state: ").upper()
+while short_code != "":
     try:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+        print(short_code, "is", CODE_TO_NAME[short_code])
     except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+    short_code = input("Enter short state: ").upper()
