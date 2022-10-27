d03835b6bbdb590e9abf2ea722a7ac5f47c780f8
David C. Page <davidpage876@gmail.com>
2022-10-27 16:05:30 +1000

    Add sample test code for ProgrammingLanguage class

---
+++ prac_06/languages.py
@@ -0,0 +1,10 @@
+"""Programming Language exercise - using ProgrammingLanguage class
+Time expected: 15 min
+       actual:
+"""
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
