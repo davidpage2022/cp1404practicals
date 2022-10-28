00b828863023824ff8b152da465d48542cb225bd
David C. Page <davidpage876@gmail.com>
2022-10-28 10:08:38 +1000

    Add tests for Guitar class

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,20 @@
+"""Guitar exercise
+Time expected: 10 min
+       actual: 8 min
+"""
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Tests for Guitar class."""
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another = Guitar("Another Guitar", 2013, 3200.20)
+
+    print(f"{gibson.name} get_age() - Expected 100. Got {gibson.get_age()}")
+    print(f"{another.name} get_age() - Expected 9. Got {another.get_age()}")
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another.name} is_vintage() - Expected False. Got {another.is_vintage()}")
+
+
+if __name__ == '__main__':
+    main()
