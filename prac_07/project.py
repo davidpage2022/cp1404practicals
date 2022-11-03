d931145f050e17d3d549628030845998e4240b71
David C. Page <davidpage876@gmail.com>
2022-11-03 16:48:09 +1000

    Add placeholder stubs for main program functionality

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,20 +3,24 @@
 class Project:
     """Describes information about a project"""
 
-    def __init__(self, name: str, date: str, priority: int, cost_estimate: float, completion_percentage: float):
+    def __init__(self, name: str, date: str, priority: int, cost_estimate: float, percent_completed: float):
         """Initialize a project.
 
         name: Project title.
-        date: Start date string in DD/MM/YYYY format.
+        date: Start date string in DD/MM/YYYY format.  TODO: Use built-in date format.
         priority: Number indicating priority (e.g. 1 is highest priority, followed by 2, then 3...etc).
         cost_estimate: Estimated cost of the project.
-        completion_percentage: Value between 0 - 100 indicating percentage completed.
+        percent_completed: Value between 0 - 100 indicating percentage completed.
         """
         self.name = name
         self.date = date
         self.priority = priority
         self.cost_estimate = cost_estimate
-        self.completion = completion_percentage
+        self.percent_completed = percent_completed
 
     def __str__(self):
         return ""  # TODO
+
+    def is_complete(self) -> bool:
+        """ If the project is 100% completed. """
+        return False
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,3 +2,110 @@
 Estimated: 70
 Actual: 14
 """
+import datetime
+from operator import attrgetter
+
+from prac_07.project import Project
+
+FILENAME = "projects.txt"
+MENU = """- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit"""
+
+
+def main():
+    """Allow user to load, save and manage a list of projects."""
+    projects = load_projects(FILENAME)
+
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            filename = input("Filename: ")
+            load_projects(filename)  # ???
+        elif choice == "S":
+            filename = input("Filename: ")
+            save_projects(projects, filename)  # ???
+        elif choice == "D":
+            display_projects()
+        elif choice == "F":
+            date = get_date("Show projects that start after date (dd/mm/yy): ")
+            filtered_projects = filter_projects_by_date(projects, date)
+            for project in filtered_projects:
+                print(project)
+        elif choice == "A":
+            project = get_new_project()
+            projects.append(project)
+        elif choice == "U":
+            for i, project in enumerate(projects):
+                print(f"{i} {project}")
+            project = get_project(projects)
+            print(project)
+            update_project(project)
+            percent_completed = get_percentage("New percentage: ")
+            priority = get_positive_integer("New priority: ")
+            if percent_completed != "":
+                project.percent_completed = percent_completed
+            if priority != "":
+                project.priority = priority
+        print(MENU)
+        choice = input(">>> ").upper()
+    save_projects(projects, FILENAME)
+    print("Thank you for using custom-built project management software.")
+
+
+def load_projects(filename) -> list[Project]:
+    """"""
+    return []
+
+
+def save_projects(projects, filename):
+    """"""
+    pass
+
+
+def get_new_project() -> Project:
+    """"""
+    pass
+
+
+def get_project(projects) -> Project:
+    """"""
+    pass
+
+
+def get_date(param) -> datetime.date:
+    """"""
+    pass
+
+
+def get_percentage(prompt) -> float:
+    pass
+
+
+def get_positive_integer(prompt) -> int:
+    pass
+
+
+def filter_projects_by_date(projects, date) -> list[Project]:
+    """"""
+    filtered_projects = [project for project in projects if project.date > date]
+    return sorted(filtered_projects, key=attrgetter(date))
+
+
+def display_projects():
+    """"""
+    pass
+
+
+def update_project(project):
+    """"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
