a89de148ea32744d86ea93f50c25a1fa8ca7ffdf
David C. Page <davidpage876@gmail.com>
2022-11-15 10:58:38 +1000

    Refactor to keep dry and improve docstring coverage

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -8,29 +8,39 @@ MILES_TO_KM = 1.60934
 
 
 class MilesToKmConverter(App):
+    """Kivy App for converting miles to kilometres."""
     output_km_text = StringProperty()
 
     def build(self):
+        """Build the Kivy app from the kv file."""
         self.title = "Miles to KM Converter"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
     def handle_convert(self):
-        try:
-            miles = float(self.root.ids.input_miles.text)
-        except ValueError:
-            miles = 0
+        """Handle request for conversion.
+        Takes input from miles input field and outputs to km label."""
+        miles = self.get_valid_miles()
         km = miles * MILES_TO_KM
         self.output_km_text = f"{km:.3f}"
 
     def handle_increment(self, amount):
-        try:
-            miles = float(self.root.ids.input_miles.text)
-        except ValueError:
-            miles = 0
+        """Handle request for miles input field to be increment/decremented by amount.
+        Updates conversion to match the new value."""
+        miles = self.get_valid_miles()
         miles += amount
         self.root.ids.input_miles.text = str(miles)
+
+        # Update conversion.
         self.handle_convert()
+
+    def get_valid_miles(self):
+        """Get miles (as a decimal value) from user input field.
+        If the value is invalid returns zero."""
+        try:
+            return float(self.root.ids.input_miles.text)
+        except ValueError:
+            return 0
 
 
 if __name__ == '__main__':
